c668baef644a7c3af5d52691494a003d8002dcb5
ashwin <ashwin7h7@gmail.com>
2026-03-10 22:33:21 +0530

    added testSocket,js file

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 // Routes
+//http://localhost:4000/
 app.get("/", (req, res) => res.send("API is Working"));
 app.use("/api/user", userRouter);
 app.use("/api/product", productRoutes);
